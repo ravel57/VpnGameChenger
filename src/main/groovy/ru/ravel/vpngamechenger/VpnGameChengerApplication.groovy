2cc8e3a41a7a781abf8ab525b43d3bfb126b649c
petr <petyalom@yandex.ru>
2024-03-16 22:28:00 +0300

    first commit

---
+++ src/main/groovy/ru/ravel/vpngamechenger/VpnGameChengerApplication.groovy
@@ -0,0 +1,13 @@
+package ru.ravel.vpngamechenger
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class VpnGameChengerApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(VpnGameChengerApplication, args)
+	}
+
+}
